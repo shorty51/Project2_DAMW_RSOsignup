0642c3749f15f671d9c6355b5e389fc10136ebaf
shorty51 <shortnsweet5187@gmail.com>
2014-12-02 13:31:54 -0600

    added comments to updated functions

--- RSOsignup.js
+++ RSOsignup.js
@@ -1,12 +1,16 @@
 var x = "";
+var item = document.getElementById("listNames").selectedIndex;
+//document.getElementById("listNames").innerHTML = code;
+//document.getElementById("name").value = "something";
+//var name = document.getElementById("name").value;
 
 function checkMajor() {
-	
+	//if major=="other" then Inform your Major:
 	
 }
 
 function addParticipant() {
-	
+	//check fields, if empty, alert()
 	
 }
 
@@ -28,37 +32,38 @@ function sortArray() {
 }
 
 function updateList() {
-	
+	//document.getElementById("genderMale").checked
 	
 }
 
 function updateSummary() {
-	
+	//after participant added, add to select list
 	
 }
 
 function getPersonData() {
-	
+	//when name in list selected, pull array id num to display info
 	
 }
 
 function resetForm() {
-	
+	//clear all fields
 	
 }
 
 function updateParticipant() {
-	
+	//when name selected and info displayed, if changed then update
+	//similar to add participant, update the array, sort, etc
 	
 }
 
 function removeParticipant(del) {
-	
+	//update list, update array, sort list
 	
 }
 
 function cancelForm() {
-	
+	//cancels the update???? calls function reset, hides buttons
 	
 }
 
